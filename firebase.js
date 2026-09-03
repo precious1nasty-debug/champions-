d674701da74aa16ac2d65cde8c6b26f4d5e56e8b
precious1nasty-debug <precious1nasty@gmail.com>
2026-09-03 04:08:38 +0100

    Add Firestore initialization to firebase.js

--- firebase.js
+++ firebase.js
@@ -19,3 +19,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
